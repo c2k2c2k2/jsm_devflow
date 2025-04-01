252d025d800514f608625ab3b564c3698e922a40
Chaitanya <chaitanya@zitics.com>
2025-04-02 00:06:14 +0530

    eslint and prettier setup

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn"],
+  tailwindAttributes: ["className", "class"],
+};
+
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,51 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["components/ui/**/*"],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier", // Keep Prettier, but remove Tailwind ESLint plugin
+  ),
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "comma-dangle": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
