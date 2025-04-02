5d43e17653775c02eba325ff4f62aaf7539a6550
Chaitanya <chaitanya@zitics.com>
2025-04-02 17:10:11 +0530

    update: assets and metadata

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
+
 const config = {
-  plugins: ["@tailwindcss/postcss", "postcss-import",
-    "tailwindcss",
-    "autoprefixer"],
-};
+  plugins: {
+    "@tailwindcss/postcss": {}
+  }
+}
 
 export default config;
