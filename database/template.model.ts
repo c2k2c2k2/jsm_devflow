0ee67b60df1eff798447d50471af9adcfee76a71
Chaitanya <chaitanya@zitics.com>
2025-04-18 14:25:53 +0530

    Implemented question and answer model along with template model for future refs

---
+++ database/template.model.ts
@@ -0,0 +1,10 @@
+import { model, models, Schema, Types, Document } from "mongoose";
+
+export interface IModel {}
+
+export interface IModelDoc extends IModel, Document {}
+const ModelSchema = new Schema<IModel>({}, { timestamps: true });
+
+const Model = models?.Question || model<IModel>("Model", ModelSchema);
+
+export default Model;
