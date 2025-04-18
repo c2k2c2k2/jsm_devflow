8f6ae222d81930f155f2caf112c0206e1b1f2579
Chaitanya <chaitanya@zitics.com>
2025-04-18 17:16:48 +0530

    implemented logging

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/route";
+import handleError from "@/lib/handlers/error";
 import Link from "next/link";
 const questions = [
   {
@@ -59,7 +60,19 @@ const questions = [
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
+
+const test = async () => {
+  try {
+    throw new Error("This is a test error");
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 const Home = async ({ searchParams }: SearchParams) => {
+  const result = await test();
+  console.log(result);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
